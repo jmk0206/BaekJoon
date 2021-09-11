5156a5396765b7027d5295cba394d768ac0b15b9
jmk0206 <jmk960206@gmail.com>
2021-09-11 11:00:12 +0900

    Part3 for문 완료

--- BaekJoon/src/Part3/Main.java
+++ BaekJoon/src/Part3/Main.java
@@ -3,6 +3,7 @@ package Part3;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -73,20 +74,119 @@ public class Main {
 		
 		// N 찍기 (2741번)
 		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		
+//		int N = Integer.parseInt(br.readLine());
+//		br.close();
+//        
+//		int i = 1;
+//		while(i<= N) {
+//			System.out.println(i);
+//			i++;
+//		}	
+		
+		
+		// 기찍 N (2742번)
+		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		
+//		int N = Integer.parseInt(br.readLine());
+//		br.close();
+//		
+//		int i=N;
+//		while(i>0) {
+//			System.out.println(i);
+//			i--;
+//		}
+
+		
+		// A + B - 7 (11021번)
+		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		
+//		int a = Integer.parseInt(br.readLine());
+//		
+//		StringTokenizer st;
+//		for (int i=1; i<=a; i++) {
+//			st = new StringTokenizer(br.readLine(), " ");
+//			System.out.println("Case #" + i + ": "
+//					+(Integer.parseInt(st.nextToken())
+//					+Integer.parseInt(st.nextToken())));
+//		}
+//		br.close();
+
+
+		// A + B - 8 (11022번)
+		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		
+//		int C = Integer.parseInt(br.readLine());
+//		int A;
+//		int B;
+//		
+//		StringBuilder sb = new StringBuilder();
+//		StringTokenizer st;
+//		
+//		for (int i=1; i<=C; i++) {
+//			st = new StringTokenizer(br.readLine(), " ");
+//			A = Integer.parseInt(st.nextToken());
+//			B = Integer.parseInt(st.nextToken());
+//			
+//			sb.append("Case #" + i + ": ").append(A + " + " + B + " = " + (A+B)).append("\n");
+//		}
+//		br.close();
+//		System.out.println(sb);
+		
+		
+		// 별 찍기 - 1 (2438번)
+		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int N = Integer.parseInt(br.readLine());
+//		br.close();
+//		
+//		for(int i=1; i<=N; i++) {
+//			for(int j=1; j<=i; j++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		// 별 찍기 - 2 (2439번)
+		
+//		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//		int N = Integer.parseInt(br.readLine());
+//		br.close();
+//		
+//		for (int i=1; i<=N; i++) {
+//			for(int j=1; j<=N-i; j++) {
+//				System.out.print(" ");
+//			}
+//			for(int k=1; k<=i; k++) {
+//				System.out.print("*");
+//			}
+//			System.out.println();
+//		}
+		
+		
+		// X보다 작은 수 (10871번)
+		
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		StringBuilder sb = new StringBuilder();
 		
-		int N = Integer.parseInt(br.readLine());
-		br.close();
-        
-		int i = 1;
-		while(i<= N) {
-			System.out.println(i);
-			i++;
-		}	
+		int N = Integer.parseInt(st.nextToken());
+		int X = Integer.parseInt(st.nextToken());
 		
-
-
-
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		for (int i=0; i<N; i++) {
+			int value = Integer.parseInt(st.nextToken());
+			
+			if (value < X)
+				sb.append(value).append(" ");
+		}
+		System.out.println(sb);
 	}
 
 }
